52e12694157d360768c9dbb37ede31a25007308e
jazminima <92978315+Jazminima@users.noreply.github.com>
2024-11-05 17:23:01 +0000

    Update paths, Meg, mysql updates

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,27 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/docs/buildonminima/contracts/:path*',
+        destination: 'docs/development/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/buildonminima/dapps/mdsjs',
+        destination: 'docs/development/:path*',
+        permanent: true,
+      },
+      /docs/buildonminima/dapps/mdsjs
+
+    ]
+  },
+
 };
+
 
 export default withMDX(config);
